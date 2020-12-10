818ba4264e9f9492695d8e5f17748f97ae30618b
CaCaOKim <wmas2002kw@naver.com>
2020-12-10 21:15:49 +0900

    KthDivisor
    K번째 약수 구하기

---
+++ Section2/KthDivisor.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("KthDivisorInput.txt","rt")
+N, K=map(int, input().split())
+cnt = 0
+
+for i in range(1,N+1):
+    if N%i==0:
+        cnt += 1
+    if cnt==K:
+        print(i)
+        break
+else:
+    print(-1)
