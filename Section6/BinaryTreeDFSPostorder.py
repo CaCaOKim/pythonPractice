8a57894535670ae4994d99bdc46b2dcb461a6492
CaCaOKim <wmas2002kw@naver.com>
2021-03-18 20:53:32 +0900

    BinaryTreePostorder

--- Section6/BinaryTreeDFSPostorder.py
+++ Section6/BinaryTreeDFSPostorder.py
@@ -1,12 +1,12 @@
-def DFS(s):
-    if s>7:
+def DFS(s,max):
+    if s>max:
         return
     else:
-        DFS(s*2)
-        DFS(s*2+1)
+        DFS(s*2,max)
+        DFS(s*2+1,max)
         print(s, end=' ')
 
 
-DFS(1)
+DFS(1,7)
 print()
 
