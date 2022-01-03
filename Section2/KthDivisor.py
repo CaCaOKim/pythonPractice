5f43983fc69d3c8bf88a2f79316b581c3d4c5eb0
CaCaOKim <wmas2002kw@naver.com>
2022-01-03 14:03:18 +0900

    restart

--- Section2/KthDivisor.py
+++ Section2/KthDivisor.py
@@ -3,11 +3,3 @@ sys.stdin = open("KthDivisorInput.txt","rt")
 N, K=map(int, input().split())
 cnt = 0
 
-for i in range(1,N+1):
-    if N%i==0:
-        cnt += 1
-    if cnt==K:
-        print(i)
-        break
-else:
-    print(-1)
