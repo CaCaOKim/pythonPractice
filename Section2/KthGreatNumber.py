ecd89c44c06433c486313df8d8fe51020dbe89e8
CaCaOKim <wmas2002kw@naver.com>
2022-01-03 14:14:26 +0900

    restart

--- Section2/KthGreatNumber.py
+++ Section2/KthGreatNumber.py
@@ -1,18 +1,7 @@
 import sys
 sys.stdin=open("KthGreatNumberInput.txt", "rt")
 
-N, K = map(int, input().split())
-nums = list(map(int, input().split()))
-sums = set()
 
-for i in range(N-2):
-    for j in range(i+1,N-1):
-        for k in range(j+1,N):
-            sums.add(nums[i]+nums[j]+nums[k])
-
-sums = list(sums)
-sums.sort(reverse=True)
-print(sums[K-1])
 
 
 
