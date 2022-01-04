cb2dfa66db7d319899205e60b28b1acdfff00587
CaCaOKim <wmas2002kw@naver.com>
2022-01-04 22:59:37 +0900

    K번째 수

--- Section2/KthNumber.py
+++ Section2/KthNumber.py
@@ -1,8 +1,13 @@
 import sys
 sys.stdin=open("KthNumberInput.txt", "rt")
 
-
-
+T = int(input())
+for t in range(T):
+    N, S, E, K = map(int, input().split())
+    temp = list(map(int, input().split()))
+    temp = temp[S-1:E]
+    temp.sort()
+    print('#' + str(t+1), temp[K-1])
 
 
 
