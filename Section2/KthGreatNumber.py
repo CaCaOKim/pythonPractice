02ee8e45d8e4f8b146b07865b0a20524c2281677
CaCaOKim <wmas2002kw@naver.com>
2022-01-05 23:55:43 +0900

    K번째 큰 수

--- Section2/KthGreatNumber.py
+++ Section2/KthGreatNumber.py
@@ -1,10 +1,16 @@
 import sys
 sys.stdin=open("KthGreatNumberInput.txt", "rt")
 
-
-
-
-
+N, K = map(int, input().split())
+cards = list(map(int, input().split()))
+sums = set()
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            sums.add(cards[i] + cards[j] + cards[k])
+sums = list(sums)
+sums.sort(reverse=True)
+print(sums[K-1])
 
 
 
