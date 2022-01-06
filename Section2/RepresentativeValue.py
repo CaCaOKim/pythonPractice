39d8e9cdce4124931b9c7b07d77724858c965834
CaCaOKim <wmas2002kw@naver.com>
2022-01-07 00:37:42 +0900

    대표값

--- Section2/RepresentativeValue.py
+++ Section2/RepresentativeValue.py
@@ -1,3 +1,22 @@
 import sys
 sys.stdin=open("RepresentativeValueInput.txt", "rt")
 
+N = int(input())
+scores = list(map(int, input().split()))
+
+result = 0
+repNum = 0
+avg = round(sum(scores)/N)
+
+for i in range(len(scores)):
+    gap = abs(scores[i] - avg)
+    gapMin = abs(repNum - avg)
+    if gap < gapMin: 
+        repNum = scores[i]
+        result = i + 1
+    elif gap == gapMin: 
+        if repNum < scores[i]:
+            repNum = scores[i]
+            result = i + 1
+
+print(avg, result)
