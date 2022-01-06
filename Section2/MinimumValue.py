da1d98156de090b0615ded25aa2f07e22bc302a8
CaCaOKim <wmas2002kw@naver.com>
2022-01-06 23:34:03 +0900

    최소값

--- Section2/MinimumValue.py
+++ Section2/MinimumValue.py
@@ -1,3 +1,6 @@
 arr = [23,5,3,4,12,1,9,123,36,3,5,2,72,6,8]
-arrMin = float('inf')
-
+result = float('inf') ## 파이썬에서 가장 큰 값
+for i in range(len(arr)):
+    if arr[i] < result:
+        result = arr[i]
+print(result)
