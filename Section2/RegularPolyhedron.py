ffbc94802504e462e62b785550af322150520249
CaCaOKim <wmas2002kw@naver.com>
2022-01-07 23:40:04 +0900

    정다면체

--- Section2/RegularPolyhedron.py
+++ Section2/RegularPolyhedron.py
@@ -1,3 +1,14 @@
 import sys
 sys.stdin=open("RegularPolyhedronInput.txt", "rt")
 
+N, M = map(int, input().split())
+nm = [N, M]
+sums = list()
+
+nm.sort
+for sum in range(nm[0]+1, nm[1]+2):
+    sums.append(sum)
+
+for x in sums:
+    print(x, end=' ')
+print()
