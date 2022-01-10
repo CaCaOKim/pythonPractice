8f64bc5f1227b5b4412ebc4f9ed859207b7f9344
CaCaOKim <wmas2002kw@naver.com>
2022-01-10 18:40:58 +0900

    자리수의 합

--- Section2/SumOfPositionalNumbers.py
+++ Section2/SumOfPositionalNumbers.py
@@ -1,5 +1,32 @@
+def digit_sum(x):
+    num = x % 10
+    nextNum = x // 10
+    sum = 0
+
+    if nextNum > 0:
+        sum = num + digit_sum(nextNum)
+    else:
+        sum += num
+
+    return sum
 
 import sys
 sys.stdin=open("SumOfPositionalNumbersInput.txt", "rt")
 
+N = int(input())
+nums = list(map(int, input().split()))
+result = list()
+maxSum = 0
 
+for x in nums: 
+    sum = digit_sum(x)
+    if sum > maxSum:
+        maxSum = sum
+        result = [x]
+    elif sum == maxSum:
+        result.append(x)
+
+for y in result:
+    print(y, end=' ')
+
+print()
