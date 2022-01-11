f63ea4a868a3d6645a370522107fc828db88f229
CaCaOKim <wmas2002kw@naver.com>
2022-01-11 21:25:46 +0900

    소수의 개수

--- Section2/CountPrimeNumberEratosthenesSieve.py
+++ Section2/CountPrimeNumberEratosthenesSieve.py
@@ -1,3 +1,14 @@
 import sys
 sys.stdin=open("CountPrimeNumberEratosthenesSieveInput.txt", "rt")
 
+N = int(input())
+nums = [0] * (N+1)
+result = 0
+
+for i in range(2, N+1):
+    if nums[i] == 0:
+        result += 1
+        for j in range(i, N+1, i):
+            nums[j] += 1
+
+print(result)
