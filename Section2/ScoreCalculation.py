5b040b997e02445f253fe317376125f6b97d4b3d
CaCaOKim <wmas2002kw@naver.com>
2022-01-14 07:18:22 +0900

    점수계산

--- Section2/ScoreCalculation.py
+++ Section2/ScoreCalculation.py
@@ -1,3 +1,16 @@
 import sys
 sys.stdin = open("ScoreCalculationInput.txt", "rt")
 
+N = int(input())
+quiz = list(map(int, input().split()))
+mark = 0
+score = 0
+
+for x in quiz:
+    if x == 0:
+        mark = 0
+    elif x == 1:
+        mark += 1
+        score += mark
+
+print(score)
