1a2aa60f76a9287c6654286788f0a34b2c936949
CaCaOKim <wmas2002kw@naver.com>
2022-01-17 07:39:45 +0900

    회문 문자열 검사

--- Section3/CircularLetterCheck.py
+++ Section3/CircularLetterCheck.py
@@ -1,3 +1,14 @@
 import sys
 sys.stdin = open("CircularLetterCheckInput.txt", "rt")
 
+N = int(input())
+
+for i in range(N):
+    word = input().lower()
+    # if word == word[::-1]: # 단어를 리버스시키는 함수
+    for j in range(len(word)//2):
+        if word[j] != word[-j-1]:
+            print('#' + str(i+1) + ' ' + 'NO')
+            break
+    else:
+        print('#' + str(i+1) + ' ' + 'YES')
