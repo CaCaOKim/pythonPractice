cbab5413045c533d62722f0a3ecf40e6c5ab090e
CaCaOKim <wmas2002kw@naver.com>
2022-01-19 00:18:22 +0900

    카드 역배치

--- Section3/CardReverseArrangement.py
+++ Section3/CardReverseArrangement.py
@@ -1,3 +1,12 @@
 import sys
 sys.stdin = open("CardReverseArrangementInput.txt", "rt")
 
+cards = list(range(1, 21))
+for _ in range(10):
+    ai, bi = map(int, input().split())
+    for i in range((bi-ai)//2 + 1):
+        cards[ai-1+i], cards[bi-1-i] = cards[bi-1-i], cards[ai-1+i]
+
+for x in cards:
+    print(x, end = ' ')
+print()
