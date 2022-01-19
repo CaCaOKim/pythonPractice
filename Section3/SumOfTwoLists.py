638f09fcc5513d9b5e2ffb3da8f3f768bc0ef40b
CaCaOKim <wmas2002kw@naver.com>
2022-01-20 00:14:21 +0900

    두 리스트 합치기

--- Section3/SumOfTwoLists.py
+++ Section3/SumOfTwoLists.py
@@ -1,6 +1,15 @@
 import sys
 sys.stdin = open("SumOfTwoListsInput.txt", "rt")
 
+N = int(input())
+nList = list(map(int, input().split()))
+M = int(input())
+mList = list(map(int, input().split()))
 
+result = nList + mList
+result.sort()
 
+for x in result:
+    print(x, end=' ')
+print()
 
