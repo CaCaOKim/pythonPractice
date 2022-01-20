1bc4b8c3c402cf0e911fa77629e54341e09b2748
CaCaOKim <wmas2002kw@naver.com>
2022-01-21 02:03:54 +0900

    수의 함

--- Section3/SumNumbers.py
+++ Section3/SumNumbers.py
@@ -1,3 +1,25 @@
 import sys
 sys.stdin = open("SumNumbersInput.txt", "rt")
 
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+cnt = 0
+sum = 0
+sidx = 0
+
+for i in range(N):
+    sum += nums[i]
+    if sum > M:
+        while sum > M:
+            sum -= nums[sidx]
+            sidx += 1
+            if sum == M:
+                cnt += 1
+    elif sum == M:
+        sum -= nums[sidx]
+        sidx += 1
+        cnt += 1
+        
+
+    
+print(cnt)
