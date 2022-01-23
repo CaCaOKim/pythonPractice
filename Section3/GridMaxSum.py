f754b418be90f47f6cb7f9dc070c34b66698f0ea
CaCaOKim <wmas2002kw@naver.com>
2022-01-24 01:49:12 +0900

    격자판 최대합

--- Section3/GridMaxSum.py
+++ Section3/GridMaxSum.py
@@ -1,4 +1,29 @@
 import sys
 sys.stdin = open("GridMaxSumInput.txt", "rt")
 
+N = int(input())
+grid = []
+for _ in range(N):
+    grid.append(list(map(int, input().split())))
+max = 0
+sumld = 0
+sumrd = 0
 
+for i in range(N):
+    sumh = 0
+    sumv = 0
+    for j in range(N):
+        sumh += grid[i][j]
+        sumv += grid[j][i]
+    if sumh > max:
+        max = sumh
+    if sumv > max:
+        max = sumv
+    sumld += grid[i][i]
+    sumrd += grid[i][N-1-i]
+if sumld > max:
+    max = sumld
+if sumrd > max:
+    max = sumrd
+
+print(max)
