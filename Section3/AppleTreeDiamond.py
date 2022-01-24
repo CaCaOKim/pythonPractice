cc1b9d966d6b4ab9ee03710b17607b7654a3e576
CaCaOKim <wmas2002kw@naver.com>
2022-01-25 07:53:56 +0900

    사과나무(다이아몬드)

--- Section3/AppleTreeDiamond.py
+++ Section3/AppleTreeDiamond.py
@@ -1,2 +1,17 @@
 import sys
 sys.stdin = open("AppleTreeDiamondInput.txt", "rt")
+
+N = int(input())
+maxCnt = N//2
+result = 0
+
+for i in range(N):
+    apple = list(map(int, input().split()))
+    if i <= maxCnt:
+        for j in range(maxCnt-i, maxCnt+i+1):
+            result += apple[j]
+    else:
+        for k in range(i-maxCnt, N-i+maxCnt):
+            result += apple[k]
+
+print(result)
