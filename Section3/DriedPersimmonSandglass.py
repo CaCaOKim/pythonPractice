671debe5880bb7728a1c18ac1ea5549223fb5c4c
CaCaOKim <wmas2002kw@naver.com>
2022-01-26 01:20:45 +0900

    곳감(모래시계)

--- Section3/DriedPersimmonSandglass.py
+++ Section3/DriedPersimmonSandglass.py
@@ -1,2 +1,28 @@
+from re import M
 import sys
 sys.stdin = open("DriedPersimmonSandglassInput.txt", "rt")
+
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
+M = int(input())
+
+for _ in range(M):
+    turn = list(map(int, input().split()))
+    idx = turn[0]-1
+    if turn[1] == 0:
+        for _ in range(turn[2]):
+            grid[idx].append(grid[idx].pop(0))
+    if turn[1] == 1:
+        for _ in range(turn[2]):
+            grid[idx].insert(0, grid[idx].pop(N-1))
+            
+result = 0
+for i in range(N):
+    if i <= N//2:
+        for j in range(i, N-i):
+            result += grid[i][j]
+    else:
+        for k in range(N-1-i, i+1):
+            result += grid[i][k]
+
+print(result)
