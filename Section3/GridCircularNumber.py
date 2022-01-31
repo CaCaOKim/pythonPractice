7721217065027e959835b47b49481faf4ceb7a49
CaCaOKim <wmas2002kw@naver.com>
2022-01-31 12:12:10 +0900

    격자판 회문수

--- Section3/GridCircularNumber.py
+++ Section3/GridCircularNumber.py
@@ -1,3 +1,29 @@
+def checkCir(arr):
+    result = True
+    for idx in range(len(arr)//2):
+        if arr[idx] != arr[len(arr)-1-idx]:
+            result = False
+            break
+    return result
+
 import sys
 sys.stdin = open("GridCircularNumberInput.txt", "rt")
+
+grid = [list(map(int, input().split())) for _ in range(7)]
+cnt = 0
+
+for i in range(7):
+    for j in range(3):
+        hnums = []
+        vnums = []
+        for k in range(5):
+            hnums.append(grid[i][j+k])
+            vnums.append(grid[j+k][i])
+        if checkCir(hnums):
+            cnt += 1
+        if checkCir(vnums):
+            cnt += 1
+
+print(cnt)
+
 
