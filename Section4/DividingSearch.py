ce921393589ba785ca0a9443dbc3533dd56aac7e
CaCaOKim <wmas2002kw@naver.com>
2022-02-01 14:52:58 +0900

    이분 검색

--- Section4/DividingSearch.py
+++ Section4/DividingSearch.py
@@ -1,3 +1,22 @@
 import sys
 sys.stdin = open("DividingSearchInput.txt", "rt")
 
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+nums.sort()
+
+# print(nums.index(M) + 1)
+
+li = 0
+ri = N - 1
+
+while li <= ri:
+    mi = (li+ri)//2
+    if nums[mi] > M:
+        ri = mi - 1
+    elif nums[mi] < M:
+        li = mi + 1
+    else:
+        print(mi + 1)
+        break
