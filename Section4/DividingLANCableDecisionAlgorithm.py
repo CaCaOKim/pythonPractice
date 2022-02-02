4684684dbe566786cc27563c1661bae8d3b42fd0
CaCaOKim <wmas2002kw@naver.com>
2022-02-02 15:30:16 +0900

    랜선 자르기(결정알고리즘)

--- Section4/DividingLANCableDecisionAlgorithm.py
+++ Section4/DividingLANCableDecisionAlgorithm.py
@@ -1,3 +1,28 @@
+def cntLines(arr, l):
+    count = 0
+    for x in arr:
+        count += (x//l)
+    return count
+
 import sys
 sys.stdin = open("DividingLANCableDecisionAlgorithmInput.txt", "rt")
 
+K, N = map(int, input().split())
+lans = []
+for _ in range(K):
+    lans.append(int(input()))
+
+lv = 0
+rv = max(lans)
+result = 0
+
+while lv <= rv:
+    leng = (lv + rv)//2
+    cnt = cntLines(lans, leng)
+    if cnt >= N:
+        result = leng
+        lv = leng + 1
+    else:
+        rv = leng - 1
+
+print(result)
