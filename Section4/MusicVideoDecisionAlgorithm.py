ed605a466b28f11fae955dfa923f2df0a6fe5c2c
CaCaOKim <wmas2002kw@naver.com>
2022-02-03 22:50:37 +0900

    뮤직비디오(결정알고리즘)

--- Section4/MusicVideoDecisionAlgorithm.py
+++ Section4/MusicVideoDecisionAlgorithm.py
@@ -1,4 +1,26 @@
 import sys
 sys.stdin = open("MusicVideoDecisionAlgorithmInput.txt", "rt")
 
+N, M = map(int, input().split())
+musics = list(map(int, input().split()))
 
+lv = 0
+rv = sum(musics)
+result = 0
+
+while lv <= rv:
+    mv = (lv + rv)//2
+    temp = 0
+    cnt = 1
+    for x in musics:
+        temp += x
+        if temp > mv:
+            cnt += 1
+            temp = x
+    if cnt > M:
+        lv += 1
+    else:
+        result = mv
+        rv -= 1
+
+print(result)
