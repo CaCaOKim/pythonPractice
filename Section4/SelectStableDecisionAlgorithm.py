d76df1041383b567f12cd25433d95acb943f9f99
CaCaOKim <wmas2002kw@naver.com>
2022-02-04 23:27:07 +0900

    마구간 정하기(결정알고리즘)

--- Section4/SelectStableDecisionAlgorithm.py
+++ Section4/SelectStableDecisionAlgorithm.py
@@ -1,7 +1,29 @@
 import sys
 sys.stdin = open("SelectStableDecisionAlgorithmInput.txt", "rt")
 
+N, C = map(int, input().split())
+stable = []
+for _ in range(N):
+    stable.append(int(input()))
+stable.sort()
+lv = 1
+rv = stable[N-1]
 
+while lv <= rv:
+    mv = (lv + rv)//2
+    cnt = 1
+    temp = stable[0]
+    for i in range(1, N):
+        if stable[i] - temp >= mv:
+            cnt += 1
+            temp = stable[i]
+    if cnt >= C:
+        result = mv
+        lv = mv + 1
+    else:
+        rv = mv - 1
+
+print(result)
  
 
 
