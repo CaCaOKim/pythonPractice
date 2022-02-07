c4d953fced88648fb8b5ae6df18062f033c0cd84
CaCaOKim <wmas2002kw@naver.com>
2022-02-07 23:52:06 +0900

    회의실 배정

--- Section4/MeetingRoomAllocationGridy.py
+++ Section4/MeetingRoomAllocationGridy.py
@@ -1,7 +1,20 @@
 import sys
 sys.stdin = open("MeetingRoomAllocationGridyInput.txt", "rt")
 
+N = int(input())
+mt = []
+for i in range(N):
+    s, e = map(int, input().split())
+    mt.append((s, e))
+mt.sort(key=lambda x : (x[1], x[0]))
+et = 0
+cnt = 0
+for s, e in mt:
+    if s >= et:
+        et = e
+        cnt += 1
 
+print(cnt)
 
 
 
