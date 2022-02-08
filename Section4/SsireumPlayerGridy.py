523793b266948003b24dc37c529576b57a5ff65f
CaCaOKim <wmas2002kw@naver.com>
2022-02-08 22:38:15 +0900

    씨름선수(그리디)

--- Section4/SsireumPlayerGridy.py
+++ Section4/SsireumPlayerGridy.py
@@ -1,3 +1,19 @@
 import sys
 sys.stdin = open("SsireumPlayerGridyInput.txt", "rt")
 
+N = int(input())
+players = []
+for _ in range(N):
+    h, w = map(int, input().split())
+    players.append((h, w))
+players.sort(reverse=True)
+
+cnt = 0
+maxW = 0
+
+for h, w in players:
+    if w > maxW:
+        cnt += 1
+        maxW = w
+
+print(cnt)
