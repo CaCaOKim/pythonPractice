7fb0977ebe7562490e71eefbb12205def734ce39
CaCaOKim <wmas2002kw@naver.com>
2022-02-10 00:05:13 +0900

    침몰하는 타이타닉(그리디)

--- Section4/TitanicGridy.py
+++ Section4/TitanicGridy.py
@@ -1,7 +1,25 @@
 import sys
 sys.stdin = open("TitanicGridyInput.txt", "rt")
 
+N, M = map(int, input().split())
+weight = list(map(int, input().split()))
 
+weight.sort()
+lw = 0
+rw = N-1
+boat = 0
+
+while lw <= rw:
+    boat += 1
+    if lw == rw:
+        break
+    if weight[lw] + weight[rw] > M:
+        rw -= 1
+    elif weight[lw] + weight[rw] <= M:
+        lw += 1
+        rw -= 1
+
+print(boat)
 
 
 
