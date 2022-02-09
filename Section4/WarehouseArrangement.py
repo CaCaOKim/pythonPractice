dec8a7d2f8e70cbc703a773406163dc7d1a1b00e
CaCaOKim <wmas2002kw@naver.com>
2022-02-09 23:27:54 +0900

    창고 정리(그리디)

--- Section4/WarehouseArrangement.py
+++ Section4/WarehouseArrangement.py
@@ -1,2 +1,14 @@
 import sys
 sys.stdin = open("WarehouseArrangementInput.txt", "rt")
+
+L = int(input())
+boxes = list(map(int, input().split()))
+M = int(input())
+
+for _ in range(M):
+    boxes.sort()
+    boxes[0] += 1
+    boxes[L-1] -= 1
+
+boxes.sort()
+print(boxes[L-1] - boxes[0])
