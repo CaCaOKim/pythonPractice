6453ab7acdc36199019060ed3da6dbb05eb8f474
CaCaOKim <wmas2002kw@naver.com>
2022-02-15 00:33:21 +0900

    역수열(그리디)

--- Section4/ReverseSequenceGridy.py
+++ Section4/ReverseSequenceGridy.py
@@ -1,4 +1,15 @@
 import sys
 sys.stdin = open("ReverseSequenceGridyInput.txt", "rt")
 
+N = int(input())
+seq = list(map(int, input().split()))
+result = []
 
+while N > 0:
+   result.insert(seq[N - 1], N)
+   N -= 1
+
+for x in result:
+    print(x, end=' ')
+
+print() 
