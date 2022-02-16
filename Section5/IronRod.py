c6444ed37455f2c912b869f0be647be1e6cb735e
CaCaOKim <wmas2002kw@naver.com>
2022-02-17 07:31:52 +0900

    쇠막대기

--- Section5/IronRod.py
+++ Section5/IronRod.py
@@ -1,3 +1,19 @@
 import sys
 sys.stdin = open("IronRodInput.txt", "rt")
 
+rod = input()
+
+cnt = 0
+result = 0
+
+for i in range(len(rod)):
+    if rod[i] == '(':
+        cnt += 1
+    elif rod[i] == ')':
+        cnt -= 1
+        if rod[i-1] == '(':
+            result += cnt
+        elif rod[i-1] == ')':
+            result += 1
+
+print(result)
