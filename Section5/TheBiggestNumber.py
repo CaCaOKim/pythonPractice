d6fbf64a3190a4d8248db6ce0ab5dca127dd45c7
CaCaOKim <wmas2002kw@naver.com>
2022-02-16 17:47:36 +0900

    가장 큰 수(스택)

--- Section5/TheBiggestNumber.py
+++ Section5/TheBiggestNumber.py
@@ -1,4 +1,23 @@
 import sys
 sys.stdin = open("TheBiggestNumberInput.txt", "rt")
 
+num, m = map(int, input().split())
+nums = list(map(int, str(num)))
+stack = []
+
+for _ in range(len(nums)):
+    while stack and stack[-1] < nums[0]:
+        stack.pop()
+        m -= 1
+    stack.append(nums[0])
+    nums.pop(0)
+if m != 0:
+    stack=stack[:-m]
+
+result = 0
+for x in stack:
+    result = result*10 + int(x)
+
+print(result)
+
 
