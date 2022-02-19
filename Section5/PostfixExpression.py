4a42037c9f748bdd7000a52a89f1c209c994e367
CaCaOKim <wmas2002kw@naver.com>
2022-02-19 11:19:08 +0900

    후위표기식 만들기

--- Section5/PostfixExpression.py
+++ Section5/PostfixExpression.py
@@ -1,7 +1,34 @@
+from cmath import sin
 import sys
+from unicodedata import decimal
 sys.stdin = open("PostfixExpressionInput.txt", "rt")
 
+exp = input()
+sign = []
+result = ""
 
+for x in exp:
+    if x.isdigit():
+        result += x
+    else:
+        if x == '(':
+            sign.append(x)
+        elif x == '*' or x == '/':
+            while sign and (sign[-1] == '*' or x == '/'):
+                result += sign.pop()
+            sign.append(x)
+        elif x == '+' or x == '-':
+            while sign and sign[-1] != '(':
+                result += sign.pop()
+            sign.append(x)
+        elif x == ')':
+            while sign and sign[-1] != '(':
+                result += sign.pop()
+            sign.pop()
 
+while sign:
+    result += sign.pop()
+
+print(result)
 
 
