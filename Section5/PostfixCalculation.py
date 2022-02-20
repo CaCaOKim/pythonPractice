3c3e587d2d6d5c9a577c4939cb0bd67764e6036a
CaCaOKim <wmas2002kw@naver.com>
2022-02-21 07:45:43 +0900

    후위 연산

--- Section5/PostfixCalculation.py
+++ Section5/PostfixCalculation.py
@@ -1,5 +1,29 @@
+from curses.ascii import isdigit
 import sys
 sys.stdin = open("PostfixCalculationInput.txt", "rt")
 
+exp = input()
+nums = []
 
-
+for x in exp:
+    if x.isdigit():
+        nums.append(int(x))
+    else: 
+        if x == '+':
+            b = nums.pop()
+            a = nums.pop()
+            nums.append(a + b)
+        elif x == '-':
+            b = nums.pop()
+            a = nums.pop()
+            nums.append(a - b)
+        elif x == '*':
+            b = nums.pop() 
+            a = nums.pop()
+            nums.append(a * b)
+        elif x == '/':
+            b = nums.pop()
+            a = nums.pop()
+            nums.append(a / b)
+            
+print(nums[0])
