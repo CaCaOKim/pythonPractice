77eac1579756feed0a147d85c0e2215442fd6234
CaCaOKim <wmas2002kw@naver.com>
2022-02-23 01:15:47 +0900

    응급실(큐)

--- Section5/EmergencyRoom.py
+++ Section5/EmergencyRoom.py
@@ -1,3 +1,17 @@
 import sys
 sys.stdin = open("EmergencyRoomInput.txt", "rt")
 
+N, M = map(int, input().split())
+patients = [(idx, dng) for idx, dng in enumerate(list(map(int, input().split())))] 
+cnt = 0
+
+while len(patients) > 1:
+    if any(patients[0][1] < x[1] for x in patients):
+        patients.append(patients.pop(0))
+    else:
+        maxNum = patients.pop(0)
+        cnt += 1
+        if maxNum[0] == M:
+            break
+
+print(cnt)
