bc3f077cdb168f07e35d19bd8bedb1e2aa635aae
CaCaOKim <wmas2002kw@naver.com>
2022-02-22 23:46:04 +0900

    공주구하기(큐)

--- Section5/SavePrincess.py
+++ Section5/SavePrincess.py
@@ -1,4 +1,14 @@
 import sys
 sys.stdin = open("SavePrincessInput.txt", "rt")
 
+N, K = map(int, input().split())
+princes = []
+for i in range(1, N+1):
+    princes.append(i)
 
+while len(princes) > 1:
+    for j in range(K-1):
+        princes.append(princes.pop(0))
+    princes.pop(0)
+
+print(princes[0])
