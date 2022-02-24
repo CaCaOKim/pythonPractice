9e8a6b05e44fabe670533fd24603d100db0459cd
CaCaOKim <wmas2002kw@naver.com>
2022-02-24 21:49:20 +0900

    교육과정설계(큐)

--- Section5/CurriculumPlaning.py
+++ Section5/CurriculumPlaning.py
@@ -1,3 +1,24 @@
 import sys
 sys.stdin = open("CurriculumPlaningInput.txt", "rt")
 
+nece = input()
+cnt = int(input())
+result = []
+
+for _ in range(cnt):
+    curriculum = input()
+    temp = nece
+    for word in curriculum:
+        if temp[0] == word:
+            temp = temp[1:]
+            if len(temp) == 0:
+                break
+    if temp == '':
+        result.append('YES')
+    else:
+        result.append('NO')
+
+for i in range(cnt):
+    print('#' + str(i+1) + ' ' + result[i])
+    
+
