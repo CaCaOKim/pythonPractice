37316a8765f9fc127956ec498db54ebd5f44fe89
CaCaOKim <wmas2002kw@naver.com>
2022-02-25 23:19:50 +0900

    단어찾기(해쉬)

--- Section5/WordSearching.py
+++ Section5/WordSearching.py
@@ -1,4 +1,12 @@
 import sys
 sys.stdin = open("WordSearchingInput.txt", "rt")
 
+N = int(input())
+words = []
 
+for _ in range(N):
+    words.append(input())
+for _ in range(N-1):
+    words.remove(input())
+
+print(words[0])
