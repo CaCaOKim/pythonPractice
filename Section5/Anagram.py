860aca9c4be5e975e2cba2b5d5aff2ac35cab19c
CaCaOKim <wmas2002kw@naver.com>
2022-02-26 11:29:51 +0900

    아나그램

--- Section5/Anagram.py
+++ Section5/Anagram.py
@@ -1,3 +1,19 @@
 import sys
 sys.stdin = open("AnagramInput.txt", "rt")
 
+w1 = input()
+w2 = input()
+dic = dict()
+
+for x in w1:
+    dic[x] = dic.get(x, 0) + 1
+for x in w2:
+    dic[x] = dic.get(x, 0) - 1
+
+for x in w1:
+    if dic[x] > 0:
+        print("NO")
+        break
+else:
+    print("YES")
+    
