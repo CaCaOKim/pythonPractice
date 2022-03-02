ae302fd8fb32ee6d9a6e85f08327a4661d756341
CaCaOKim <wmas2002kw@naver.com>
2022-03-02 22:55:06 +0900

    최소힙

--- Section5/MinimumHeap.py
+++ Section5/MinimumHeap.py
@@ -1,3 +1,18 @@
 import sys
+import heapq
 sys.stdin = open("MinimumHeapInput.txt", "rt")
+
+nums = []
+while True:
+    N = int(input())
+    if N == -1:
+        break
+    if N == 0:
+        if len(nums) == 0:
+            print(-1)
+        else:
+            print(heapq.heappop(nums))
+    else:
+        heapq.heappush(nums, N)
+
 
