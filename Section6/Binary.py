11adf3907b52de8158661dece6a30680ff0cba2e
CaCaOKim <wmas2002kw@naver.com>
2022-03-14 22:10:45 +0900

    재귀함수를 이용한 이진수 출력

--- Section6/Binary.py
+++ Section6/Binary.py
@@ -1,4 +1,11 @@
+def binary(num):
+    if num > 1:
+        return binary(num//2) + str(num%2)
+    else:
+        return str(num%2)
+
 import sys
 sys.stdin = open("BinaryInput.txt", "rt")
 
-
+N = int(input())
+print(binary(N))
