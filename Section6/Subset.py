fc612b702452640dc45615f92d97df29df688cad
CaCaOKim <wmas2002kw@naver.com>
2022-03-18 07:39:09 +0900

    부분집합 구하기(DFS)

--- Section6/Subset.py
+++ Section6/Subset.py
@@ -1,5 +1,19 @@
+def DFS(num, arr, lim):
+    if num == lim+1:
+        for i in range(1, lim+1):
+            if arr[i] == 1:
+                print(i, end=' ')
+        print()
+    else:
+        arr[num]=1
+        DFS(num+1, arr, lim)
+        arr[num]=0
+        DFS(num+1, arr, lim)
+
+
 import sys
 sys.stdin = open("SubsetInput.txt", "rt")
 
-
-
+N = int(input())
+temp = [0]*(N+1)
+DFS(1, temp, N)
