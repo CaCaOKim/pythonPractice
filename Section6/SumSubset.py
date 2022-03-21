8cc6e3cded66bad3fff9b6d0af69a6ce31984bf5
CaCaOKim <wmas2002kw@naver.com>
2022-03-21 22:35:13 +0900

    합이 같은 부분집합(DFS)

--- Section6/SumSubset.py
+++ Section6/SumSubset.py
@@ -1,4 +1,20 @@
+def DFS(n, s, cnt, arr, tot):
+    if s > tot//2:
+        return
+    if n == cnt:
+        if s == (tot-s):
+            print("YES")
+            sys.exit(0)
+    else:
+        DFS(n+1, s+arr[n], cnt, arr, tot)
+        DFS(n+1, s, cnt, arr, tot)
+
 import sys
 sys.stdin = open("SumSubsetInput.txt", "rt")
 
+N = int(input())
+nums = list(map(int, input().split()))
 
+total = sum(nums)
+DFS(0, 0, N, nums, total)
+print("NO")
