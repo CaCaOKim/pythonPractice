4f61dd7c597e31d91481409d877365ba58d118be
CaCaOKim <wmas2002kw@naver.com>
2022-03-22 23:26:34 +0900

    바둑이 승차(DFS)

--- Section6/RidingWithDogs.py
+++ Section6/RidingWithDogs.py
@@ -1,4 +1,23 @@
+def DFS(i, sum, temp, mw, cnt, arr, tot):
+    global result
+    if sum + (tot-temp) < result:
+        return
+    if sum > mw:
+        return
+    if i == cnt:
+        if sum > result:
+            result = sum
+    else:
+        DFS(i+1, sum+arr[i], temp+arr[i], mw, cnt, arr, tot)
+        DFS(i+1, sum, temp+arr[i], mw, cnt, arr, tot)
+
 import sys
 sys.stdin = open("RidingWithDogsInput.txt", "rt")
 
+C, N = map(int, input().split())
+dogs = list(int(input()) for _ in range(N))
+total = sum(dogs)
+result = -2147000000
 
+DFS(0, 0, 0, C, N, dogs, total)
+print(result)
