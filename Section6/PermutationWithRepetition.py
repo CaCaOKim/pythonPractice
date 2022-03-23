53ea1c2b011f20fdb5eeb4d4c123aec88520e84e
CaCaOKim <wmas2002kw@naver.com>
2022-03-23 23:45:24 +0900

    중복순열 구하기(DFS)

--- Section6/PermutationWithRepetition.py
+++ Section6/PermutationWithRepetition.py
@@ -1,4 +1,21 @@
+def DFS(cnt, arr, max, mcnt):
+    global result
+    if cnt == mcnt:
+        for j in range(mcnt):
+            print(arr[j], end=' ')
+        print()
+        result += 1
+    else:
+        for i in range(1, max+1):
+            arr[cnt] = i
+            DFS(cnt+1, arr, max, mcnt)
+
 import sys
 sys.stdin = open("PermutationWithRepetitionInput.txt", "rt")
 
+N, M = map(int, input().split())
+result = 0
+numArr = [0]*M
 
+DFS(0, numArr, N, M)
+print(result)
