3c321473f5bb0e24fe954fbc729111968ea4b7b1
CaCaOKim <wmas2002kw@naver.com>
2022-03-24 23:41:29 +0900

    동전교환 미완

--- Section6/CoinChange.py
+++ Section6/CoinChange.py
@@ -1,6 +1,18 @@
+from lib2to3.pgen2 import grammar
+
+
+def DFS(typeCnt, coinArr, remains):
+    global cnt
+    
+
 import sys
 sys.stdin = open("CoinChangeInput.txt", "rt")
 
+N = int(input())
+coins = list(map(int, input().split()))
+M = int(input())
+cnt = 0
 
-
+DFS(N, coins, M)
+print(cnt)
 
