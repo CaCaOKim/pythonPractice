9a97453ac769f43a22c955137a70636cc4f47318
CaCaOKim <wmas2002kw@naver.com>
2022-03-25 23:32:12 +0900

    동전교환

--- Section6/CoinChange.py
+++ Section6/CoinChange.py
@@ -1,18 +1,28 @@
 from lib2to3.pgen2 import grammar
 
 
-def DFS(typeCnt, coinArr, remains):
+def DFS(c, sum, typeCnt, coinArr, remains):
     global cnt
-    
+    if c > cnt:
+        return
+    if sum > remains:
+        return
+    if sum == remains:
+        if c < cnt:
+            cnt = c
+    else:
+        for i in range(typeCnt):
+            DFS(c+1, sum+coinArr[i], typeCnt, coinArr, remains)
 
 import sys
 sys.stdin = open("CoinChangeInput.txt", "rt")
 
 N = int(input())
 coins = list(map(int, input().split()))
+coins.sort(reverse=True)
 M = int(input())
-cnt = 0
+cnt = 2147000000
 
-DFS(N, coins, M)
+DFS(0, 0, N, coins, M)
 print(cnt)
 
