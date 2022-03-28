22ea36066457e4fd6f5f27adca3fc19cda837cdd
CaCaOKim <wmas2002kw@naver.com>
2022-03-28 22:33:51 +0900

    순열 구하기(DFS)

--- Section6/Permutation.py
+++ Section6/Permutation.py
@@ -1,4 +1,24 @@
+def DFS(num, arr, chArr, n, m):
+    global cnt
+    if num == m:
+        for i in range(num):
+            print(arr[i], end=' ')
+        print()
+        cnt += 1
+    else:
+        for j in range(1, n+1):
+            if chArr[j] == 0:
+                chArr[j] = 1
+                arr[num] = j
+                DFS(num+1, arr, chArr, n, m)
+                chArr[j] = 0
+
 import sys
 sys.stdin = open("PermutationInput.txt", "rt")
 
-
+N, M = map(int, input().split())
+marbles = [0]*N
+check = [0]*(N+1)
+cnt = 0
+DFS(0, marbles, check, N, M)
+print(cnt)
